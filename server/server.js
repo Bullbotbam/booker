be1aae976e90fbe37cdf8bac19b3635cebf4122c
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-09 04:46:37 -0500

    correct server.js errors to deploy to heroku correctly

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -15,7 +16,7 @@ const startServer = async () => {
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		// context: authMiddleware,
+		context: authMiddleware,
 	});
 
 	// Start the Apollo server
@@ -33,10 +34,12 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booker', {
-	useFindAndModify: false,
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
