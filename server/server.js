df5227f8f27341826ff1f7b57bb896af2eee8f86
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-09 05:29:38 -0500

    heroku errors on server.js, following errors to fix issue

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const mongoose = require('mongoose');
 const path = require('path');
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -34,6 +38,7 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -43,7 +48,5 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-	});
+	app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
